7210e696b2bcd8f000e11d703eb7ec29b164c253
Petar Marinov <eng.petar.marinov@gmail.com>
2024-07-17 16:23:11 +0300

    more time

--- time/time.rb
+++ time/time.rb
@@ -14,4 +14,24 @@ threads = 1.times.map do
 end
 
 puts "[#{Time.now}]This is main thread"
+
+# Add 5 minutes
+future_time_minutes = current_time + (5 * 60)
+puts "Future Time (after 5 minutes): #{future_time_minutes}"
+
+# Add 3 hours
+future_time_hours = current_time + (3 * 60 * 60)
+puts "Future Time (after 3 hours): #{future_time_hours}"
+
+# Add 2 days
+future_time_days = current_time + (2 * 24 * 60 * 60)
+puts "Future Time (after 2 days): #{future_time_days}"
+
+formatted_time = current_time.strftime('%Y-%m-%d %H:%M:%S')
+puts "Formatted Time: #{formatted_time}"
+
+unix_timestamp = current_time.to_i
+puts "Current Unix Timestamp: #{unix_timestamp}"
+
+# wait for all threads to finish
 threads.each(&:join)
